6f640be5afd6d0ba3c2e9ea2728e8f5ceb25ca70
naomijub <jnaomi@thoughtworks.com>
2019-07-20 16:36:08 -0300

    Adds pins to game

--- bowling/src/bowling/frame.clj
+++ bowling/src/bowling/frame.clj
@@ -2,3 +2,9 @@
 
 (defn start-game []
   (vector))
+
+(defn add-turn-pins [v pin-1 pin-2]
+  (conj v [pin-1 pin-2]))
+
+
+(add-turn-pins (start-game) 7 2)
--- bowling/test/bowling/frame_test.clj
+++ bowling/test/bowling/frame_test.clj
@@ -5,4 +5,12 @@
 (deftest frame-test
   (testing "frame is generated as empty vector"
     (is (vector? (start-game)))
-    (is (= 0 (count (start-game))))))
+    (is (= 0 (count (start-game)))))
+  (testing "vector has new scores"
+    (let [score-0 (start-game)
+          score-1 (add-turn-pins score-0 7 2)
+          score-2 (add-turn-pins score-1 10 0)
+          score-3 (add-turn-pins score-2 7 3)]
+      (is (= [[7 2]] score-1))
+      (is (= [[7 2] [10 0]] score-2))
+      (is (= [[7 2] [10 0] [7 3]] score-3)))))
