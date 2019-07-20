f4d2adf5cb8ce6405aba13cfcb123574f1d32ad8
naomijub <jnaomi@thoughtworks.com>
2019-07-20 18:10:15 -0300

    Includes spare case - I know condp isn;t the most performatic way to solve this, but it is the wuickest

--- bowling/src/bowling/score.clj
+++ bowling/src/bowling/score.clj
@@ -1,1 +1,23 @@
 (ns bowling.score)
+
+(defn sum-numeral-values [n]
+  (let [numbers (clojure.string/split n #" ")]
+    (->> numbers
+      (map #(Integer/parseInt %))
+      (reduce +))))
+
+(defn sum-spare [head next]
+  (let [next-value (first (clojure.string/split next #""))]
+    (+ 10 (Integer/parseInt next-value))))
+
+(defn get-score [result] 
+  (loop [results result scores []]
+    (let [head (first results) tail (rest results)]
+      (if (empty? results)
+        scores
+        (let [current-score 
+              (cond 
+                (clojure.string/includes? head "/") (conj scores (sum-spare head (first tail)))
+                :else (conj scores (sum-numeral-values head)))]
+          (recur tail current-score))))))
+                   
--- bowling/test/bowling/score_test.clj
+++ bowling/test/bowling/score_test.clj
@@ -1,4 +1,28 @@
 (ns bowling.frame-test
   (:require [clojure.test :refer :all]
             [bowling.score :refer :all]
-            [bowling.frame :as f]))
+            [bowling.frame :refer [start-game]]))
+
+(deftest score-test
+  (testing "empty vector is empty result"
+    (is (= [] (get-score (start-game)))))
+  (testing "Different numeral interactions"
+    (let [score-1 ["3 4"]
+          score-2 ["3 4" "5 3"]
+          score-3 ["3 4" "5 3" "2 6"]]
+      (is (= [7] (get-score score-1)))
+      (is (= [7 8] (get-score score-2)))
+      (is (= [7 8 8] (get-score score-3)))))
+  (testing "Testing with spae"
+    (let [score-2 ["3/" "5 3"]
+          score-3 ["3/" "5 3" "2 6"]]
+      (is (= [15 8] (get-score score-2)))
+      (is (= [15 8 8] (get-score score-3))))))
+
+(deftest score-types-test
+  (testing "When score is all numeral returns the integer sum"
+    (is (= 7 (sum-numeral-values "3 4")))
+    (is (= 8 (sum-numeral-values "6 2"))))
+  (testing "When the head item is a spare"
+    (is (= 17 (sum-spare "3/" "7 2")))
+    (is (= 13 (sum-spare "6/" "3/")))))
