7ec475bdcfec3a38ce99a88fa660bf896aa61d9c
naomijub <jnaomi@thoughtworks.com>
2019-07-20 21:39:28 -0300

    Adapting the for the last frame

--- bowling/src/bowling/frame.clj
+++ bowling/src/bowling/frame.clj
@@ -3,15 +3,29 @@
 (defn start-game []
   (vector))
 
-(defn add-turn-pins [v pin-1 pin-2]
-  (if (>= (count v) 10)
-    (throw (Exception. "Cannot play more than 10 turns"))
-    (conj v [pin-1 pin-2])))
+(defn add-turn-pins [v pin-1 pin-2 & rst]
+  (cond
+    (>= (count v) 10) (throw (Exception. "Cannot play more than 10 turns"))
+    (and (= (count v) 9) (or (= pin-1 10) (= 10 (+ pin-1 pin-2)))) (conj v [pin-1 pin-2 (first rst)])
+    (and (= (count v) 9) (and (not (= pin-1 10)) (not (= 10 (+ pin-1 pin-2))))) (throw (Exception. "Cannot play 3 rounds in the tenth turn with open frame"))
+    :else (conj v [pin-1 pin-2])))
+
+(defn last-result [score]
+  (let [first (first score) 
+        second (second score) 
+        third (last score)]
+    (if (= 10 first)
+      (str "X" " " second " " third)
+      (if (= 10 (+ first second))
+        (str first "/" third)
+        (str first " " second)))))
 
 (defn get-printable-result [v]
-  (mapv 
-    (fn [score]
-      (cond 
-        (= 10 (get score 0)) "X"
-        (= 10 (+ (get score 0) (get score 1))) (str (get score 0) "/") 
-        :else (str (get score 0) " " (get score 1)))) v))  
+  (apply vector 
+    (map-indexed
+      (fn [idx score]
+        (cond
+          (and (= 9 idx) (= 3 (count score))) (last-result score)
+          (= 10 (get score 0)) "X"
+          (= 10 (+ (get score 0) (get score 1))) (str (get score 0) "/") 
+          :else (str (get score 0) " " (get score 1)))) v)))
--- bowling/test/bowling/frame_test.clj
+++ bowling/test/bowling/frame_test.clj
@@ -21,5 +21,17 @@
       (is (= (count expected) (count score)))))
   (testing "Exception is thrown when try to add extra results after 10 turns"
     (let [scores [[7 2] [10 0] [7 3] [7 2] [10 0] [7 3] [7 2] [10 0] [7 3] [3 4]]]
-      (is (thrown? Exception (add-turn-pins scores 4 5))))))
+      (is (thrown? Exception (add-turn-pins scores 4 5))))
+    (let [scores [[7 2] [10 0] [7 3] [7 2] [10 0] [7 3] [7 2] [10 0] [7 3]]]
+      (is (thrown? Exception (add-turn-pins scores 4 5 4)))))
+  (testing "at last turn get an extra turn if pin-1 = X or pin-2 = /"
+    (let [scores [[7 2] [10 0] [7 3] [7 2] [10 0] [7 3] [7 2] [10 0] [7 3]]
+          tenth-turn (add-turn-pins scores 10 7 2)]
+      (is (= [[7 2] [10 0] [7 3] [7 2] [10 0] [7 3] [7 2] [10 0] [7 3] [10 7 2]] tenth-turn))
+      (is (= ["7 2" "X" "7/" "7 2" "X" "7/" "7 2" "X" "7/" "X 7 2"] (get-printable-result tenth-turn))))))
 
+(deftest last-result-test
+  (testing "last values with strikes and spares"
+    (is (= "X 7 2" (last-result [10 7 2])))
+    (is (= "3/2" (last-result [3 7 2])))
+    (is (= "4 5" (last-result [4 5 6])))))
