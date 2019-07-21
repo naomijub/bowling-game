6873aa2dc2917b661a9e0f537acdec770bcb6afc
naomijub <jnaomi@thoughtworks.com>
2019-07-21 02:28:14 -0300

    Includes spare scores

---
+++ bow/src/score.rs
@@ -0,0 +1,69 @@
+fn get_scores(scores: &mut Vec<String>) -> Vec<i32> {
+  recur_scores(scores.swap_remove(0), scores, &mut Vec::new())
+}
+
+fn recur_scores(head: String, tail:  &mut Vec<String>, scores: &mut Vec<i32>) -> Vec<i32> {
+  if tail.len() <= 0 { return scores.clone(); }
+  match &head {
+    // h if h.contains("X") => , 
+    head if head.contains("/") => {
+      let head_score = count_spare(head.to_owned(), &tail.first());
+      scores.push(head_score);
+      recur_scores(tail.swap_remove(0), tail, scores)
+    },
+    _ => {
+      let head_score = count_numerals(head);
+      scores.push(head_score);
+      recur_scores(tail.swap_remove(0), tail, scores)
+    }
+  }
+}
+
+pub fn count_spare(_head: String, next: &Option<&String>) -> i32 {
+  if next.unwrap().contains("X") {
+    return 20;
+  } else if next.unwrap().contains("/") {
+    let s = next.unwrap().chars().into_iter().map(|c| c.to_string()).collect::<Vec<String>>();
+    return 10 + s.first().unwrap().parse::<i32>().unwrap();
+  } else {
+    let v: Vec<&str> = next.unwrap().split(' ').collect();
+    10 + v.first().unwrap().parse::<i32>().unwrap()
+  }
+}
+
+pub fn count_numerals(head: String) -> i32 {
+  let v: Vec<&str> = head.split(' ').collect();
+  v.into_iter().map(|n| n.parse::<i32>().unwrap()).fold(0,|acc,n| acc + n)
+}
+
+#[cfg(test)]
+mod tests {
+  use super::{count_numerals, count_spare};
+
+  #[test]
+  fn counts_numeral_score() {
+    let value = count_numerals(String::from("5 3"));
+    assert_eq!(value, 8);
+  }
+
+  #[test]
+  fn counts_spare_score_followed_by_numerals() {
+    let next = &String::from("7 2");
+    let value = count_spare(String::from("5/"), &Some(next));
+    assert_eq!(value, 17);
+  }
+
+  #[test]
+  fn counts_spare_score_followed_by_spare() {
+    let next = &String::from("6/");
+    let value = count_spare(String::from("5/"), &Some(next));
+    assert_eq!(value, 16);
+  }
+
+  #[test]
+  fn counts_spare_score_followed_by_strike() {
+    let next = &String::from("X");
+    let value = count_spare(String::from("5/"), &Some(next));
+    assert_eq!(value, 20);
+  }
+}
